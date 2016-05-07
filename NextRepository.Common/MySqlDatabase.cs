630887296a357edc7f66a469fa4b940986fab4a3
mcvguy <me.shahidali@hotmail.com>
2016-05-07 12:56:12 +0200

    refactored classes living in same file.

--- NextRepository.Common/MySqlDatabase.cs
+++ NextRepository.Common/MySqlDatabase.cs
@@ -1,13 +1,13 @@
 ﻿using System.Data;
-using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace NextRepository.Common
 {
-    public class MsSqlDatabase : Database
+    public class MySqlDatabase : Database
     {
         public override IDbConnection GetConnection(string connectionString)
         {
-            return new SqlConnection(connectionString);
+            return new MySqlConnection(connectionString);
         }
     }
 }
